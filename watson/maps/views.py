9d4a58e7d467b3ec5c5301c556eb43e4fec27245
Avatar Azka <avatar.azka@gmail.com>
2023-08-26 12:09:46 +1000

    feat: display map on index!!!!

--- watson/maps/views.py
+++ watson/maps/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from dotenv import load_dotenv, find_dotenv
+import os
+import folium
 
 def index(request):
-    return render (request, "index.html")
+    # centered on UQ
+    m = folium.Map(location=[-27.49894, 153.01368], zoom_start=19)
+    context = {
+        'map': m._repr_html_()
+        }
+    return render (request, "index.html", context)
+
+def get_url() -> str:
+    load_dotenv(find_dotenv())
+    return os.environ["API_URL"]
