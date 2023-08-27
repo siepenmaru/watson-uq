f14e2d00a5a1d8b28ee8da2b6d0b92aa6a7edca4
Avatar Azka <avatar.azka@gmail.com>
2023-08-27 11:55:24 +1000

    fix: prevent showing classes just about to end

--- watson/api_utils/misc.py
+++ watson/api_utils/misc.py
@@ -3,13 +3,14 @@ from datetime import datetime, timedelta
 def floor_hour(t: datetime) -> datetime:
     # https://stackoverflow.com/questions/48937900/round-time-to-nearest-hour-python
     # Rounds to previous hour
-    return t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
+    # add 1 minute to prevent showing classes that just ended
+    return t.replace(second=0, microsecond=0, minute=1, hour=t.hour)
 
 def ceil_hour(t: datetime) -> datetime:
     # https://stackoverflow.com/questions/48937900/round-time-to-nearest-hour-python
-    # Rounds to previous hour
+    # Rounds to next hour
     # what. why
     return (datetime(
         2000, 1, 1,
         hour = t.hour, minute=0, second=0, microsecond=0
-        ) + timedelta(hours=1)).time()
+        ) + timedelta(minutes=59)).time()
