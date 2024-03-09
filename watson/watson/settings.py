2131db793eed811b61bff3e62c64c245dad08acb
Avatar Azka <avatar.azka@gmail.com>
2024-03-10 05:10:51 +1000

    fix: modify template code for static file deployment

--- watson/watson/settings.py
+++ watson/watson/settings.py
@@ -83,14 +83,7 @@ WSGI_APPLICATION = 'watson.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+if len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
