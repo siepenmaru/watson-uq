a8ef7ea33f1a2a9874d8e4a5c15fd6e7fafe9393
Avatar Azka <avatar.azka@gmail.com>
2024-03-10 04:44:04 +1000

    build: add deps for deployment

--- watson/watson/settings.py
+++ watson/watson/settings.py
@@ -22,12 +22,12 @@ load_dotenv(find_dotenv())
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['watson.uqcloud.net']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
